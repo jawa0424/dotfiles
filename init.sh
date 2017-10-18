00f03cd2efdc57d1dfddd1199800734b9e0f3cd6
Noritaka Aizawa <noritaka.aizawa@gmail.com>
2017-10-18 23:25:27 +0900

    NERDTreeをやめてVaffle入れてみた

---
+++ init.sh
@@ -0,0 +1,6 @@
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# symlinks
+ln -s ~/dotfiles/vimfiles ~/vimfiles
+ln -s ~/vimfiles/vimrc ~/.vimrc
